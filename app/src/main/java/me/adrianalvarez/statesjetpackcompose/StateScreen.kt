89aba7a20a71e3e28776bf9a531d8c277f5a036b
Facultad De Sistemas <sistemas@academico.unab.ad>
2026-09-24 17:45:41 -0500

    Dados

--- app/src/main/java/me/adrianalvarez/statesjetpackcompose/StateScreen.kt
+++ app/src/main/java/me/adrianalvarez/statesjetpackcompose/StateScreen.kt
@@ -1,12 +1,15 @@
 package me.adrianalvarez.statesjetpackcompose
 
+import android.nfc.Tag
 import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.lazy.staggeredgrid.rememberLazyStaggeredGridState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.contextmenu.provider.TextContextMenuDataProvider
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,32 +19,52 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+
 @Preview(showSystemUi = true)
 @Composable
 fun StateScreen(){
+    var number by remember {mutableStateOf((1..6).random())}
+    var lista = listOf(
+        R.drawable.dice_1,
+        R.drawable.dice_2,
+        R.drawable.dice_3,
+        R.drawable.dice_4,
+        R.drawable.dice_5,
+        R.drawable.dice_6
 
-    //var contador = 3
-    var contador by remember { mutableStateOf(value = 3) }
+
+    )
 
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Contador: $contador", fontSize = 32.sp)
+        Image(
+            painter = painterResource(id = lista[number - 1]),
+            contentDescription = "dado",
+            modifier = Modifier
+                .height(200.dp)
+                .clip(CircleShape)
+        )
+
+
         Spacer(modifier = Modifier.height(25.dp))
-        Button(onClick = {
-            Log.i("mis-errores", "Estoy dando Click")
-            contador = contador + 1
-        }) {
-            Text("Sumar 1")
+        Button(
+            onClick =  {
+                Log.i("mis-errores", "Estoy dando click")
+                number = (1..6).random()
+            }
+        )
+        {
+            Text("Lanzar el dado!!!!")
         }
     }
 }
-
-
 
